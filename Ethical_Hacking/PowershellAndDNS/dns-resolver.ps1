248eb36f1963a81cbefa170ca45ea36a6d39058e
Morgan H <71153236+morgan-hanrahan@users.noreply.github.com>
2022-09-17 18:06:25 -0400

    Add files via upload

---
+++ Ethical_Hacking/PowershellAndDNS/dns-resolver.ps1
@@ -0,0 +1,8 @@
+﻿param ($network,$server)
+
+
+for($i=1;$i -lt 255;$i++)
+{
+
+Resolve-DnsName -DnsOnly $network'.'$i -Server $server -ErrorAction Ignore | Select-Object -Property Name, NameHost 
+}
